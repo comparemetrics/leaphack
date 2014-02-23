b3a7ba031551b37c9dff05d9141694d48fca42ce
root <root@mint>
2014-02-22 23:02:36 -0600

    adding the rest of my crap

--- grid/app.js
+++ grid/app.js
@@ -1,1 +1,28 @@
+$(document).ready(function() {
 
+	/* This is basic - uses default settings */
+	
+	$("a#single_image").fancybox();
+	
+	/* Using custom settings */
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+
+	/* Apply fancybox to multiple items */
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
+        LeapManager.init({
+            maxCursors:2,
+            enableTouchScrolling: false,
+            enableScrollbarScrolling: true,
+        });
